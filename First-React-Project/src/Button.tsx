359120b30a51bffc3d621b35c897b229c1365b5e
intimetec-nishant-menaria <nishant.menaria@intimetec.com>
2026-02-11 16:18:27 +0530

    removing unwanted files

--- First-React-Project/src/Button.tsx
+++
@@ -1,7 +0,0 @@
-
-function Button(obj:any){
-    console.log("inside different component");
-    return <button onClick={()=> obj.onClickHandler(obj.count -1)}>{obj.text}</button>
-}
-
-export default Button
